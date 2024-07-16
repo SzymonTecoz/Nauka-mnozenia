52203b3e9ba44a82a797bce4ae96c8a7da5c8c20
Szymon Nowicki <s.nowick@wp.pl>
2024-07-16 15:52:15 +0200

    Dodano funkcję sprawdzającą poprawność odpowiedzi i zakończono główny szkielet programu

--- main.py
+++ main.py
@@ -4,10 +4,19 @@ def generate_case():
     b = random.randint(1,50)
     wynik = random.randint (10, 100)
     while wynik % b != 0:
-        b = random.randint(1,10)
+        b = random.randint(1,50)
         wynik = random.randint(10,100)
-    print(b)
-    print(wynik)
+    print(f"b = {b}")
+    print(f"wynik = {wynik}")
     correct_answer = wynik/b
+    return b, wynik, correct_answer
 
-generate_case()
+def check_correctness(b, wynik, correct_answer):
+    a = int(input("Jaka liczba pomnożona przez b daje nam wynik?\n"))
+    if correct_answer == a:
+        print("Poprawna odpowiedź")
+    else:
+        print("Błąd")
+
+b, wynik, correct_answer =generate_case()
+check_correctness(b, wynik, correct_answer)
