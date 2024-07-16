3db8b4b4e98dedd0713ba044437270aa3eda969e
Szymon Nowicki <s.nowick@wp.pl>
2024-07-16 15:19:00 +0200

    Utworzono funkcję generującą wynik i jedną z liczb mnożących

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-a =1
-b = 2
+import random
+
+def generate_case():
+    b = random.randint(1,50)
+    wynik = random.randint (10, 100)
+    while wynik % b != 0:
+        b = random.randint(1,10)
+        wynik = random.randint(10,100)
+    print(b)
+    print(wynik)
+    correct_answer = wynik/b
+
+generate_case()
